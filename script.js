e7e8ac8fb76125db058ba951110adce0f36c034d
Nadila Dwi Farida R <137867311+Nadilaff@users.noreply.github.com>
2024-04-03 14:18:08 +0700

    update index

--- script.js
+++ script.js
@@ -264,19 +264,3 @@ if(chosedfile){
 })
 
 
-// CAROUSEL ULASAN APLIKASI
-var swiper = new Swiper(".slide-content", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-      });
